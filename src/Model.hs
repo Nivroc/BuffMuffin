8c0d00e8908fb182d8bd611448ef201272d7d58e
Nivroc <iliabak@mail.ru>
2020-07-17 12:38:50 -0700

    Update Model.hs

--- src/Model.hs
+++ src/Model.hs
@@ -116,7 +116,7 @@ instance Conditionable MuffinSource where
 
     messageToSend MRAlliance c = regularContent "MRAlliance: " c
     messageToSend MRHorde content = if anyKeywordsPT ["rend ", "wcb ", "warning"] content 
-                                    then #allowedMentions .~ roles $ regularContent "<@&721396284061122560> MRHorde(rend): " content 
+                                    then regularContent "MRHorde(rend): " content -- #allowedMentions .~ roles $ regularContent "<@&721396284061122560> MRHorde(rend): " content 
                                     else regularContent "MRHorde(rend): " content
                                     where roles = Just (set #roles [(getID $ ("721396284061122560" :: Text))] def)
     messageToSend MRHeart c = regularContent "MRHeart: " c
